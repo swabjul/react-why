e9f77ec897c275fee53fec9cc87085f26348dc12
Zee Jay <zj.bulawan@gmail.com>
2025-01-13 12:38:32 +0800

    Update components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,48 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import './index.css'
 import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
+
+
+function Header() {
+  return (
+    <img src="src/assets/react.svg" alt="react logo" width="40px" />
+  )
+}
+
+function Page() {
+  return (
+    <main>
+      <h1>Reason I am excited to learn React</h1>
+      <ol>
+        <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+        <li>I am more likely to get a job as a front end developer if I know React</li>
+      </ol>
+    </main>
+  )
+}
+
+function Footer() {
+  <small>© 2024 Ziroll development. All rights reserved.</small>
+}
+
+
 root.render(
-  <h1>test</h1>
+  <>
+    <Header />
+    <Page />
+    <Footer />
+  </>
+
 )
